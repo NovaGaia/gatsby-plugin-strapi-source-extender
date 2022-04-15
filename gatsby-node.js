cdb0755104a7ba665aee3d55ae9e12d584aa6b9b
hrenaud (NovaGaïa) <renaud.heluin@novagaia.com>
2022-04-15 12:36:23 +0200

    fix MUTATIONS errors and not retreaved datas who's not been updated in strapi

--- gatsby-node.js
+++ gatsby-node.js
@@ -20,8 +20,9 @@ var _require = require('humps'),
 
 var _pluginOptions = {};
 
-exports.onPreInit = function (_, pluginOptions) {
-  console.log('Loaded gatsby-plugin-strapi-override');
+exports.onPreInit = function (_ref, pluginOptions) {
+  var reporter = _ref.reporter;
+  reporter.info('Loaded gatsby-plugin-strapi-override');
   _pluginOptions.strapiTypes = pluginOptions.strapiTypes;
   _pluginOptions.seekedTypes = [];
   _pluginOptions.seekedTypesExt = [];
@@ -30,7 +31,7 @@ exports.onPreInit = function (_, pluginOptions) {
 
   if (pluginOptions.postfix === undefined || pluginOptions.postfix === '') {
     _pluginOptions.postfix = 'Ext';
-    console.log("postfix can't be unset, fallback to", _pluginOptions.postfix);
+    reporter.warn("postfix can't be unset, fallback to " + _pluginOptions.postfix);
   }
 
   if (pluginOptions.showLog === undefined || pluginOptions.showLog === '') {
@@ -45,8 +46,8 @@ exports.onPreInit = function (_, pluginOptions) {
     _pluginOptions.seekedTypesExt.push("" + strapiTypes.type + _pluginOptions.postfix);
   }
 
-  console.log('strapiTypes to manage', _pluginOptions.seekedTypes);
-  console.log('use these types instead', _pluginOptions.seekedTypesExt);
+  reporter.info("strapiTypes to manage " + _pluginOptions.seekedTypes);
+  reporter.info("use these types instead " + _pluginOptions.seekedTypesExt);
 };
 
 function onCreateNode(_x) {
@@ -54,44 +55,58 @@ function onCreateNode(_x) {
 }
 
 function _onCreateNode() {
-  _onCreateNode = (0, _asyncToGenerator2.default)( /*#__PURE__*/_regenerator.default.mark(function _callee(_ref) {
-    var node, createNode, createNodeId, createContentDigest, POST_NODE_TYPE, _iterator2, _step2, _strapiType, _iterator3, _step3, _dz, dynamicZone, dynamicZoneArray, dynamicZoneJSON, position, _iterator4, _step4, component, _dynamicZoneArray$pus, name;
+  _onCreateNode = (0, _asyncToGenerator2.default)( /*#__PURE__*/_regenerator.default.mark(function _callee2(_ref2) {
+    var node, reporter, createNode, createNodeId, createContentDigest, POST_NODE_TYPE, _node, _iterator2, _step2, _strapiType, _iterator3, _step3, _dz, dynamicZone, dynamicZoneArray, dynamicZoneJSON, position, _iterator4, _step4, component, _dynamicZoneArray$pus, name;
 
-    return _regenerator.default.wrap(function _callee$(_context) {
+    return _regenerator.default.wrap(function _callee2$(_context2) {
       while (1) {
-        switch (_context.prev = _context.next) {
+        switch (_context2.prev = _context2.next) {
           case 0:
-            node = _ref.node, createNode = _ref.actions.createNode, createNodeId = _ref.createNodeId, createContentDigest = _ref.createContentDigest;
+            node = _ref2.node, reporter = _ref2.reporter, createNode = _ref2.actions.createNode, createNodeId = _ref2.createNodeId, createContentDigest = _ref2.createContentDigest;
 
+            if (!((node === null || node === void 0 ? void 0 : node.internal) === undefined)) {
+              _context2.next = 3;
+              break;
+            }
+
+            return _context2.abrupt("return");
+
+          case 3:
             if (node.internal.type !== null && _pluginOptions.seekedTypes.indexOf(node.internal.type) > -1) {
               POST_NODE_TYPE = "" + node.internal.type + _pluginOptions.postfix;
 
               if (_pluginOptions.showLog) {
-                console.log('Processing', POST_NODE_TYPE);
-                console.log('NodeId', POST_NODE_TYPE + "-" + node.id);
-              } // unshuffle all mixed components properties.
+                reporter.info("Processing " + POST_NODE_TYPE);
+                reporter.info("NodeId " + POST_NODE_TYPE + "-" + node.id);
+              } // reporter.info(`>>>node.internal.type ${node.internal.type`})
+              // reporter.info(`>>>POST_NODE_TYPE ${POST_NODE_TYPE}`)
+              // working on a copy of object
 
+
+              _node = JSON.parse(JSON.stringify(node)); // reporter.info(`>>>_node ${_node}`)
+              // unshuffle all mixed components properties.
+              // taking care only for selected types in _pluginOptions.strapiTypes
 
               for (_iterator2 = _createForOfIteratorHelperLoose(_pluginOptions.strapiTypes); !(_step2 = _iterator2()).done;) {
                 _strapiType = _step2.value;
 
+                // taking care only if as dynamicZones
                 if (_strapiType.type === node.internal.type && _strapiType.dynamicZones) {
+                  // taking car of the dynamic zone
                   for (_iterator3 = _createForOfIteratorHelperLoose(_strapiType.dynamicZones); !(_step3 = _iterator3()).done;) {
                     _dz = _step3.value;
 
-                    if (node[_dz] && node[_dz].length > 0) {
+                    if (_node[_dz] && _node[_dz].length > 0) {
                       dynamicZone = {};
                       dynamicZoneArray = [];
                       dynamicZoneJSON = [];
                       position = 0;
 
-                      for (_iterator4 = _createForOfIteratorHelperLoose(node[_dz]); !(_step4 = _iterator4()).done;) {
+                      for (_iterator4 = _createForOfIteratorHelperLoose(_node[_dz]); !(_step4 = _iterator4()).done;) {
                         component = _step4.value;
 
                         if (component) {
-                          // console.log('>>>component', component)
-                          name = camelize(component.strapi_component.replace('.', '_')); // component['__typename'] = pascalize(name)
-
+                          name = camelize(component.strapi_component.replace('.', '_'));
                           component['_xtypename'] = pascalize(name);
                           component['order'] = position;
                           dynamicZone["" + name] = component;
@@ -99,45 +114,71 @@ function _onCreateNode() {
                           dynamicZoneArray.push((_dynamicZoneArray$pus = {}, _dynamicZoneArray$pus[name] = component, _dynamicZoneArray$pus));
                           position++;
                         }
-                      } // node[_dz] = dynamicZone
+                      }
 
+                      _node[_dz] = dynamicZoneArray; // Keep data as JSON for fallBack
 
-                      node[_dz] = dynamicZoneArray; // Keep data as JSON for fallBack
-
-                      node[_dz + "JSON"] = JSON.parse(JSON.stringify(Object.assign({}, dynamicZoneJSON)));
+                      _node[_dz + "JSON"] = JSON.parse(JSON.stringify(Object.assign({}, dynamicZoneJSON)));
                     }
                   }
-                }
-              }
+                } // reporter.info(`${POST_NODE_TYPE} done!`)
 
-              createNode((0, _extends2.default)({}, node, {
-                id: createNodeId(POST_NODE_TYPE + "-" + node.id),
-                parent: null,
-                children: [],
-                internal: {
-                  type: POST_NODE_TYPE,
-                  content: JSON.stringify(node),
-                  contentDigest: createContentDigest(node)
-                }
-              }));
+
+                createNode((0, _extends2.default)({}, _node, {
+                  id: createNodeId(POST_NODE_TYPE + "-" + node.id),
+                  parent: null,
+                  children: [],
+                  internal: {
+                    type: POST_NODE_TYPE,
+                    content: JSON.stringify(_node),
+                    contentDigest: createContentDigest(_node),
+                    description: "Extended version of " + node.internal.type
+                  }
+                }));
+              }
             }
 
-            return _context.abrupt("return");
+            return _context2.abrupt("return");
 
-          case 3:
+          case 5:
+          case "end":
+            return _context2.stop();
+        }
+      }
+    }, _callee2);
+  }));
+  return _onCreateNode.apply(this, arguments);
+}
+
+exports.onCreateNode = onCreateNode;
+
+exports.sourceNodes = /*#__PURE__*/function () {
+  var _ref4 = (0, _asyncToGenerator2.default)( /*#__PURE__*/_regenerator.default.mark(function _callee(_ref3) {
+    var getNodes, actions, touchNode, existingNodes;
+    return _regenerator.default.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            getNodes = _ref3.getNodes, actions = _ref3.actions;
+            touchNode = actions.touchNode; // Fetch existing strapi nodes
+
+            existingNodes = getNodes().filter(function (n) {
+              return n.internal.owner === "gatsby-plugin-strapi-source-extender";
+            }); // Touch each one of them
+
+            existingNodes.forEach(function (node) {
+              return touchNode(node);
+            });
+
+          case 4:
           case "end":
             return _context.stop();
         }
       }
     }, _callee);
   }));
-  return _onCreateNode.apply(this, arguments);
-}
 
-exports.onCreateNode = onCreateNode; // exports.onCreateNode = async ({
-//   node,
-//   actions: { createNode },
-//   createNodeId,
-//   createContentDigest,
-// }) => {
-// }
+  return function (_x2) {
+    return _ref4.apply(this, arguments);
+  };
+}();
--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,8 +1,8 @@
 const { camelize, pascalize } = require('humps')
 const _pluginOptions = {}
 
-exports.onPreInit = (_, pluginOptions) => {
-  console.log('Loaded gatsby-plugin-strapi-override')
+exports.onPreInit = ({ reporter }, pluginOptions) => {
+  reporter.info('Loaded gatsby-plugin-strapi-override')
   _pluginOptions.strapiTypes = pluginOptions.strapiTypes
   _pluginOptions.seekedTypes = []
   _pluginOptions.seekedTypesExt = []
@@ -10,7 +10,9 @@ exports.onPreInit = (_, pluginOptions) => {
   _pluginOptions.showLog = pluginOptions.showLog
   if (pluginOptions.postfix === undefined || pluginOptions.postfix === '') {
     _pluginOptions.postfix = 'Ext'
-    console.log("postfix can't be unset, fallback to", _pluginOptions.postfix)
+    reporter.warn(
+      `postfix can't be unset, fallback to ${_pluginOptions.postfix}`
+    )
   }
   if (pluginOptions.showLog === undefined || pluginOptions.showLog === '') {
     _pluginOptions.showLog = false
@@ -21,41 +23,52 @@ exports.onPreInit = (_, pluginOptions) => {
       `${strapiTypes.type}${_pluginOptions.postfix}`
     )
   }
-  console.log('strapiTypes to manage', _pluginOptions.seekedTypes)
-  console.log('use these types instead', _pluginOptions.seekedTypesExt)
+  reporter.info(`strapiTypes to manage ${_pluginOptions.seekedTypes}`)
+  reporter.info(`use these types instead ${_pluginOptions.seekedTypesExt}`)
 }
 
 async function onCreateNode({
   node,
+  reporter,
   actions: { createNode },
   createNodeId,
   createContentDigest,
 }) {
+  if (node?.internal === undefined) {
+    return
+  }
   if (
     node.internal.type !== null &&
     _pluginOptions.seekedTypes.indexOf(node.internal.type) > -1
   ) {
     let POST_NODE_TYPE = `${node.internal.type}${_pluginOptions.postfix}`
     if (_pluginOptions.showLog) {
-      console.log('Processing', POST_NODE_TYPE)
-      console.log('NodeId', `${POST_NODE_TYPE}-${node.id}`)
+      reporter.info(`Processing ${POST_NODE_TYPE}`)
+      reporter.info(`NodeId ${POST_NODE_TYPE}-${node.id}`)
     }
+    // reporter.info(`>>>node.internal.type ${node.internal.type`})
+    // reporter.info(`>>>POST_NODE_TYPE ${POST_NODE_TYPE}`)
+    // working on a copy of object
+    const _node = JSON.parse(JSON.stringify(node))
+    // reporter.info(`>>>_node ${_node}`)
+
     // unshuffle all mixed components properties.
+    // taking care only for selected types in _pluginOptions.strapiTypes
     for (const _strapiType of _pluginOptions.strapiTypes) {
+      // taking care only if as dynamicZones
       if (_strapiType.type === node.internal.type && _strapiType.dynamicZones) {
+        // taking car of the dynamic zone
         for (const _dz of _strapiType.dynamicZones) {
-          if (node[_dz] && node[_dz].length > 0) {
+          if (_node[_dz] && _node[_dz].length > 0) {
             const dynamicZone = {}
             const dynamicZoneArray = []
             const dynamicZoneJSON = []
             let position = 0
-            for (const component of node[_dz]) {
+            for (const component of _node[_dz]) {
               if (component) {
-                // console.log('>>>component', component)
                 const name = camelize(
                   component.strapi_component.replace('.', '_')
                 )
-                // component['__typename'] = pascalize(name)
                 component['_xtypename'] = pascalize(name)
                 component['order'] = position
                 dynamicZone[`${name}`] = component
@@ -64,39 +77,41 @@ async function onCreateNode({
                 position++
               }
             }
-            // node[_dz] = dynamicZone
-            node[_dz] = dynamicZoneArray
+            _node[_dz] = dynamicZoneArray
             // Keep data as JSON for fallBack
-            node[`${_dz}JSON`] = JSON.parse(
+            _node[`${_dz}JSON`] = JSON.parse(
               JSON.stringify(Object.assign({}, dynamicZoneJSON))
             )
           }
         }
       }
+      // reporter.info(`${POST_NODE_TYPE} done!`)
+      createNode({
+        ..._node,
+        id: createNodeId(`${POST_NODE_TYPE}-${node.id}`),
+        parent: null,
+        children: [],
+        internal: {
+          type: POST_NODE_TYPE,
+          content: JSON.stringify(_node),
+          contentDigest: createContentDigest(_node),
+          description: `Extended version of ${node.internal.type}`,
+        },
+      })
     }
-
-    createNode({
-      ...node,
-      id: createNodeId(`${POST_NODE_TYPE}-${node.id}`),
-      parent: null,
-      children: [],
-      internal: {
-        type: POST_NODE_TYPE,
-        content: JSON.stringify(node),
-        contentDigest: createContentDigest(node),
-      },
-    })
   }
   return
 }
 
 exports.onCreateNode = onCreateNode
 
-// exports.onCreateNode = async ({
-//   node,
-//   actions: { createNode },
-//   createNodeId,
-//   createContentDigest,
-// }) => {
+exports.sourceNodes = async ({ getNodes, actions }) => {
+  const { touchNode } = actions
+  // Fetch existing strapi nodes
+  const existingNodes = getNodes().filter(
+    n => n.internal.owner === `gatsby-plugin-strapi-source-extender`
+  )
 
-// }
+  // Touch each one of them
+  existingNodes.forEach(node => touchNode(node))
+}
